bd2c7ab3bb9e57860074289235b4bb55309b440d
Bruno Bertolini <dev@brunobertolini.com>
2017-03-02 15:43:24 -0300

    fix: lint pass

--- packages/settings/index.js
+++ packages/settings/index.js
@@ -2,28 +2,28 @@ const fs = require('fs')
 const _ = require('lodash')
 const dotenv = require('dotenv')
 
-let settings = {}
+const settings = {}
 
-const load = (path) => {
-		if (!path) {
-			return settings;
+const load = path => {
+	if (!path) {
+		return settings
+	}
+
+	const envRef = fs.readFileSync(path)
+	const envVars = dotenv.parse(envRef)
+
+	_.forEach(envVars, (value, key) => {
+		const keys = key.toLowerCase().split('_')
+
+		if (keys.length > 1) {
+			settings[keys[0]] = settings[keys[0]] || {}
+			settings[keys.shift()][keys.join('_')] = value
+		} else {
+			settings[keys[0]] = value
 		}
+	})
 
-    const envRef = fs.readFileSync(path)
-    const envVars = dotenv.parse(envRef)
-
-    _.forEach(envVars, (value, key) => {
-        let keys = key.toLowerCase().split('_')
-
-        if (keys.length > 1) {
-            settings[keys[0]] = settings[keys[0]] || {}
-            settings[keys.shift()][keys.join('_')] = value
-        } else {
-            settings[keys[0]] = value
-        }
-    })
-
-    return settings
+	return settings
 }
 
-module.exports = load;
+module.exports = load
--- packages/settings/test/unit.js
+++ packages/settings/test/unit.js
@@ -2,17 +2,17 @@ import test from 'ava'
 import configLoader from './../index'
 
 test('load .env file', t => {
-	const settings = configLoader('./test/.env');
+	const settings = configLoader('./test/.env')
 
-	t.is(settings.test, 'test');
-	t.is(settings.obj.a, 'a');
-	t.is(settings.another.a_b, 'b');
+	t.is(settings.test, 'test')
+	t.is(settings.obj.a, 'a')
+	t.is(settings.another.a_b, 'b')
 })
 
 test('load get loaded settings', t => {
-	const settings = configLoader();
+	const settings = configLoader()
 
-	t.is(settings.test, 'test');
-	t.is(settings.obj.a, 'a');
-	t.is(settings.another.a_b, 'b');
+	t.is(settings.test, 'test')
+	t.is(settings.obj.a, 'a')
+	t.is(settings.another.a_b, 'b')
 })
