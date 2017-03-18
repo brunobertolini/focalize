f30d35c09d16c9e036a121ca5ff19104da9ca706
Bruno Bertolini <dev@brunobertolini.com>
2017-03-18 12:52:04 -0300

    feat: add database dependency

--- packages/focalizejs/index.js
+++ packages/focalizejs/index.js
@@ -1,9 +1,11 @@
 const server = require('focalize-server')
 const settings = require('focalize-settings')
 const router = require('focalize-router')
+const database = require('focalize-database')
 
 module.exports = {
 	server,
 	settings,
-	router: router.load
+	router: router.load,
+	database: database.connect
 }
