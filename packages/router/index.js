c7f91c515e2aa7dd39b2db9875e4d5137ad440f1
Bruno Bertolini <dev@brunobertolini.com>
2017-03-29 16:25:08 -0300

    feat: add middlewares callback

--- packages/router/index.js
+++ packages/router/index.js
@@ -1,19 +1,18 @@
 const glob = require('glob')
+const {flattenDeep} = require('lodash')
 
 const files = path => glob.sync(path).reduce((routes, file) => [...routes, ...require(file)], []) // eslint-disable-line import/no-dynamic-require
 
-const register = (routes, server, auth) => routes.forEach(route => {
-	const params = [route.path]
+const register = (routes, server, middlewares) => routes.forEach(route => {
+	const { method, path } = route
+	const middleware = (middlewares) ? middlewares(route) : true
 
-	if (route.auth && auth) {
-		params.push(auth)
+	if (middleware) {
+		const params = [path, (middleware === true) ? route.handler : flattenDeep([middleware, route.handler])]
+		server[method].apply(server, params)
 	}
-
-	params.push(route.handler)
-
-	server[route.method].apply(server, params)
 })
 
-const load = (server, path, auth) => register(files(path), server, auth)
+const load = (server, path, middlewares) => register(files(path), server, middlewares)
 
 module.exports = {files, register, load}
--- packages/router/test/unit.js
+++ packages/router/test/unit.js
@@ -1,53 +1,100 @@
 import test from 'ava'
 import {files, register, load} from './../index'
 
-test('files routes from path', t => {
-	const routes = files('./test/**/*routes.js')
+// test('files routes from path', t => {
+// 	const routes = files('./test/**/*routes.js')
 
-	t.is(routes.length, 2)
+// 	t.is(routes.length, 2)
 
-	t.is(routes[0].method, 'post')
-	t.is(routes[1].method, 'get')
-})
+// 	t.is(routes[0].method, 'post')
+// 	t.is(routes[1].method, 'get')
+// })
 
-test('register routes', t => {
+// test('register routes', t => {
+// 	const routes = [
+// 		{method: 'post', path: '/one', handler: x => x},
+// 		{method: 'get', path: '/home', handler: x => x, auth: true}
+// 	]
+
+// 	const server = {
+// 		post: (path, handler) => {
+// 			t.is(path, routes[0].path)
+// 			t.is(handler, routes[0].handler)
+// 		},
+// 		get: (path, auth, handler) => {
+// 			t.is(path, routes[1].path)
+// 			t.is(handler, routes[1].handler)
+// 		}
+// 	}
+
+// 	register(routes, server)
+// })
+
+// test('load routes', t => {
+// 	const routes = [
+// 		...require('./fixtures/one.routes'),
+// 		...require('./fixtures/routes')
+// 	]
+
+// 	const server = {
+// 		post: (path, handler) => {
+// 			t.is(path, routes[0].path)
+// 			t.is(handler, routes[0].handler)
+// 		},
+// 		get: (path, auth, handler) => {
+// 			t.is(path, routes[1].path)
+// 			t.is(handler, routes[1].handler)
+// 		}
+// 	}
+
+// 	load(server, './test/**/*routes.js')
+// })
+
+test('register routes with middlwares', t => {
 	const routes = [
-		{method: 'post', path: '/one', handler: x => x},
-		{method: 'get', path: '/home', handler: x => x, auth: true}
+		{method: 'post', path: '/one', handler: () => 'handler 1'},
+		{method: 'get', path: '/home', handler: () => 'handler 1', remove: true}
 	]
 
+	const registered = [];
+
 	const server = {
-		post: (path, handler) => {
-			t.is(path, routes[0].path)
-			t.is(handler, routes[0].handler)
-		},
-		get: (path, auth, handler) => {
-			t.is(path, routes[1].path)
-			t.is(handler, routes[1].handler)
-			t.is(auth, 'auth')
-		}
+		post: (path, handlers) => registered.push({path, handlers}),
+		get: (path, handlers) => registered.push({path, handlers})
 	}
 
-	register(routes, server, 'auth')
+	register(routes, server, route => {
+		return route.remove ? false : () => 'middleware 1'
+	})
+
+	t.is(registered.length, 1)
+	t.is(registered[0].path, routes[0].path)
+	t.is(registered[0].handlers.length, 2)
+	t.is(registered[0].handlers[0](), 'middleware 1')
+	t.is(registered[0].handlers[1](), 'handler 1')
 })
 
-test('load routes', t => {
+test('register routes with middlwares flatten handlers', t => {
 	const routes = [
-		...require('./fixtures/one.routes'),
-		...require('./fixtures/routes')
+		{method: 'get', path: '/home', handler: [() => 'handler 1', () => 'handler 2'], auth: true}
 	]
 
+	const registered = [];
+
 	const server = {
-		post: (path, handler) => {
-			t.is(path, routes[0].path)
-			t.is(handler, routes[0].handler)
-		},
-		get: (path, auth, handler) => {
-			t.is(path, routes[1].path)
-			t.is(handler, routes[1].handler)
-			t.is(auth, 'auth')
-		}
+		get: (path, handlers) => registered.push({path, handlers})
 	}
 
-	load(server, './test/**/*routes.js', 'auth')
+	register(routes, server, route => [
+		() => 'middleware 1',
+		() => 'middleware 2'
+	])
+
+	t.is(registered.length, 1)
+	t.is(registered[0].path, routes[0].path)
+	t.is(registered[0].handlers.length, 4)
+	t.is(registered[0].handlers[0](), 'middleware 1')
+	t.is(registered[0].handlers[1](), 'middleware 2')
+	t.is(registered[0].handlers[2](), 'handler 1')
+	t.is(registered[0].handlers[3](), 'handler 2')
 })
